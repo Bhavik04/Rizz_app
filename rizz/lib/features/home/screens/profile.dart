3ab23438a8c264c4517ba93d1cc10949833abf1a
Bhavik Choudhary <bhavik123667@gmail.com>
2024-02-26 01:11:24 +0530

    fix

--- rizz/lib/features/home/screens/profile.dart
+++ rizz/lib/features/home/screens/profile.dart
@@ -39,7 +39,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
     if (userData != null && userData.containsKey('photoURL')) {
       setState(() {
-        profileImageURL = userData['photoURL'];
+        profileImageURL = userData['photoURL'][0];
       });
     }
   }
