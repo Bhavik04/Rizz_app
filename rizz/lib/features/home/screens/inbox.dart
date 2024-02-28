4d9ffc31846e4d3b437b8e9f3dec74af6edf3648
Bhavik Choudhary <69357691+Bhavik04@users.noreply.github.com>
2024-02-28 11:30:20 +0530

    ui

--- rizz/lib/features/home/screens/inbox.dart
+++ rizz/lib/features/home/screens/inbox.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:rizz/common/global_variables.dart';
 import 'package:rizz/common/utils.dart';
+import 'package:rizz/features/auth/widgets/custom_text.dart';
 import 'package:rizz/features/auth/widgets/new_card_widget.dart';
 import 'package:rizz/features/auth/widgets/custom_appbar.dart';
-import 'package:rizz/features/auth/widgets/revealed_card_widget.dart';
 
 class InboxScreen extends StatefulWidget {
   static const routeName = 'InboxScreen';
@@ -16,39 +17,27 @@ class InboxScreen extends StatefulWidget {
 class _InboxScreenState extends State<InboxScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2, // Number of tabs
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: HexColor("0F0F0F"),
-          appBar: const CustomAppBar(),
-          body: Column(
-            children: [
-              const TabBar(
-                labelColor: Colors.white,
-                labelStyle: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-                tabs: [
-                  Tab(text: 'New'),
-                  Tab(text: 'Reveal'),
-                ],
-                indicatorColor: Colors.white,
-              ),
-              Expanded(
-                child: TabBarView(
-                  children: [
-                    //new tab
-                    NewCardWidget(),
-                    //reveal tab
-                    RevealedCardWidget()
-                  ],
-                ),
-              ),
-            ],
+    return Scaffold(
+      backgroundColor: HexColor("0F0F0F"),
+      appBar: const CustomAppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+                top: GlobalVariables.deviceHeight * 0.02,
+                left: GlobalVariables.deviceWidth * 0.05),
+            child: const CustomText(
+              text: 'Your ratings',
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              textColor: Colors.white,
+            ),
           ),
-        ),
+          Expanded(
+            child: NewCardWidget(),
+          ),
+        ],
       ),
     );
   }
